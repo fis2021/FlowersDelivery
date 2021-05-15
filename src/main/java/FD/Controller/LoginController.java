8103cc6bea18651b76b39860b18357d80c11b5dd
Monica <mihutescu_monica_99@yahoo.com>
2021-05-15 17:39:57 +0300

    Login 1/2

--- src/main/java/FD/Controller/LoginController.java
+++ src/main/java/FD/Controller/LoginController.java
@@ -2,14 +2,20 @@ package FD.Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
     @FXML
-    private Label logMessage;
+    private Label Status;
     @FXML
     private TextField usernameField;
     @FXML
@@ -22,10 +28,10 @@ public class LoginController {
     public void handleRegistrationView(ActionEvent actionEvent) {
     }
 
-   /*public void handleRegistrationView() throws IOException {
+   public void handleRegistrationView() throws IOException {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("register.fxml"));
         Stage scene= (Stage) logButton.getScene().getWindow();
-        scene.setTitle("Welcome");
+        scene.setTitle("GIFTINO");
         scene.setScene(new Scene(root,725,490));
     }
 
@@ -76,6 +82,6 @@ public class LoginController {
             //   logMessage.setText(e.getMessage());
         }
 
-    }*/
+    }
 
 }
